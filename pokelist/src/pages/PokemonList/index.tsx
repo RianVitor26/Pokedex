5107b5318433af0c9120e273c8e6878e84b6e027
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-21 21:56:55 -0300

    adicionando input na pagina de pokemons

--- pokelist/src/pages/PokemonList/index.tsx
+++ pokelist/src/pages/PokemonList/index.tsx
@@ -1,12 +1,17 @@
 import * as C from './styles'
 import { Navbar } from '../../components/Navbar'
+import { BsFilterCircleFill, BsSearch } from 'react-icons/bs';
 
 export const PokemonList = () => {
   return (
     <>
       <Navbar></Navbar>
-          <C.Container>
-              Lista de pokemons
+      <C.Container>
+        <C.InputContainer>
+          <input type="text" placeholder='Search by name of Pokemon'/>
+                  <BsFilterCircleFill></BsFilterCircleFill>
+                  <BsSearch></BsSearch>
+        </C.InputContainer>
       </C.Container>
     </>
   );
--- pokelist/src/pages/PokemonList/styles.tsx
+++ pokelist/src/pages/PokemonList/styles.tsx
@@ -3,4 +3,36 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 90vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input{
+        width: 300px;
+        margin-top: 2rem;
+        border-radius: .5rem;
+        padding: 0.5rem 1rem;
+    }
+`
+export const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+
+    input{
+        width: 300px;
+        margin-top: 2rem;
+        border-radius: .5rem;
+        padding: 0.5rem 1rem;
+        margin: 1rem 0;
+    }
+
+    svg{
+        color: var(--link-color);
+        font-size: clamp(1.2rem, 2.5rem, 10rem);
+        cursor: pointer;
+
+        :nth-child(2){
+            margin: 0 1rem;
+        }
+    }
 `
